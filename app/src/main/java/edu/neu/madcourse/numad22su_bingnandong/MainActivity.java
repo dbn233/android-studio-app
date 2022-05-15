2948bd80361d731312b1b11d7de9842795142f22
dbn233 <cdhdnsdbn@sina.com.cn>
2022-05-15 22:40:24 +0800

    add button

--- app/src/main/java/edu/neu/madcourse/numad22su_bingnandong/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_bingnandong/MainActivity.java
@@ -2,13 +2,23 @@ package edu.neu.madcourse.numad22su_bingnandong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-
+import android.widget.Toast;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void aboutMe(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Name:BingnanDong\nEmail:dong.bi@northeastern.edu";
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
     }
 }
